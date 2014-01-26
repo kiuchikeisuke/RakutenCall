17659ac7f7ff33467ef41ea0d1f47f41d975d516
kiuchikeisuke <minismahn@yahoo.co.jp>
2014-01-27 00:20:55 +0900

    設定用のActivityとxmlを追加

---
+++ src/jp/ne/nissing/rakutencall/PreferenceActivity.java
@@ -0,0 +1,11 @@
+package jp.ne.nissing.rakutencall;
+
+import android.os.Bundle;
+
+public class PreferenceActivity extends android.preference.PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.preference_screen);
+	}
+}
