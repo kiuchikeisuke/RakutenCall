4a0b283f9d8ac677e06d9f5873c975bfc74979c5
keisuke kiuchi <minismahn@yahoo.co.jp>
2015-10-09 16:51:59 +0900

    電話アプリが見つからなかった時にデフォルトアプリを起動できるように修正

--- src/jp/ne/nissing/rakutencall/call/CallActivity.java
+++ src/jp/ne/nissing/rakutencall/call/CallActivity.java
@@ -2,15 +2,21 @@ package jp.ne.nissing.rakutencall.call;
 
 import java.io.UnsupportedEncodingException;
 import java.net.*;
+import java.util.List;
 
 import jp.ne.nissing.rakutencall.call.condition.*;
 import jp.ne.nissing.rakutencall.preference.SharedPreferenceManager;
 import jp.ne.nissing.rakutencall.preference.phoneappdata.PhoneActivityData;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 public class CallActivity extends Activity {
 
@@ -71,6 +77,21 @@ public class CallActivity extends Activity {
 
     private void callPhone(String tel) {
         
+        Intent intent = getPhoneApp(tel);
+        
+        boolean checkImplicitIntent = checkImplicitIntent(this, intent);
+        if(checkImplicitIntent == false){
+            SharedPreferenceManager.getInstance(this).resetDefaultPhoneApp();
+            PhoneActivityData data = SharedPreferenceManager.getInstance(this).getDefaultPhoneApp();
+            Toast.makeText(this, "正しくアプリを認識できませんでした。「"+data.getApplicationName()+"」アプリを起動します", Toast.LENGTH_LONG).show();
+            intent = getPhoneApp(tel);
+        }
+        startActivity(intent);
+        
+        finish();
+    }
+
+    private Intent getPhoneApp(String tel) {
         Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(tel));
 
         PhoneActivityData data = SharedPreferenceManager.getInstance(this).getDefaultPhoneApp();
@@ -78,8 +99,16 @@ public class CallActivity extends Activity {
                 && TextUtils.isEmpty(data.getAcitivityName()) == false) {
             intent.setClassName(data.getPackageName(), data.getAcitivityName());
         }
-
-        startActivity(intent);
-        finish();
+        return intent;
+    }
+    
+    private boolean checkImplicitIntent(Context context,Intent intent){
+        boolean retval = false;
+        PackageManager pm = context.getPackageManager();
+         List<ResolveInfo> apps = pm.queryIntentActivities(intent, 0);
+        if(apps.size() > 0){
+            retval = true;
+        }
+        return retval;
     }
 }
