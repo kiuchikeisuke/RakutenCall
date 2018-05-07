5bee000c019670f47b5c5b26798d1fc69814b0d9
kiuchikeisuke <minismahn@yahoo.co.jp>
2018-05-07 17:36:54 +0900

    fix init

--- app/src/main/kotlin/jp/ne/nissing/rakutencall/utils/di/ApplicationModule.kt
+++ app/src/main/kotlin/jp/ne/nissing/rakutencall/utils/di/ApplicationModule.kt
@@ -1,7 +1,5 @@
 package jp.ne.nissing.rakutencall.utils.di
 
-import jp.ne.nissing.rakutencall.utils.commons.ExecutionThreads
-
 import android.content.Context
 import android.content.SharedPreferences
 import dagger.Module
@@ -11,9 +9,13 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import jp.ne.nissing.rakutencall.BuildConfig
 import jp.ne.nissing.rakutencall.MainApplication
 import jp.ne.nissing.rakutencall.R
-import jp.ne.nissing.rakutencall.BuildConfig
+import jp.ne.nissing.rakutencall.data.entity.contact.Contact
+import jp.ne.nissing.rakutencall.data.entity.number.PhoneNumber
+import jp.ne.nissing.rakutencall.db.DatabaseManager
+import jp.ne.nissing.rakutencall.utils.commons.ExecutionThreads
 
 /* Module for Application */
 @Module
@@ -26,7 +28,24 @@ class ApplicationModule {
 
     @Provides
     fun provideRealm(application: MainApplication): Realm {
-        val builder = RealmConfiguration.Builder().name(application.getString(R.string.app_name))
+
+        val builder = RealmConfiguration.Builder().name(APP_KEY).initialData { realm ->
+            realm.beginTransaction()
+            val contacts = DatabaseManager.getInstance(application).contacts
+            while (contacts.moveToNext()) {
+                val telNumber = contacts.getString(contacts.getColumnIndex(DatabaseManager.COL_TEL_NUMBER))
+                val displayName = contacts.getString(contacts.getColumnIndex(DatabaseManager.COL_DISPLAYNAME))
+                val contactId = contacts.getString(contacts.getColumnIndex(DatabaseManager.COL_CONTACTS_ID))
+                val contact = realm.createObject(Contact::class.java)
+                contact.contactId = contactId
+                contact.displayName = displayName
+                contact.phoneNumber = PhoneNumber(telNumber)
+                realm.insert(contact)
+            }
+            realm.commitTransaction()
+            application.deleteDatabase(DatabaseManager.DATABASE_NAME)
+
+        }
         return if (BuildConfig.DEBUG) {
             Realm.getInstance(builder.deleteRealmIfMigrationNeeded().build())
         } else {
@@ -42,5 +61,7 @@ class ApplicationModule {
         }
     }
 
-
+    companion object {
+        private const val APP_KEY = "jp.ne.nissing.rakutencall"
+    }
 }
