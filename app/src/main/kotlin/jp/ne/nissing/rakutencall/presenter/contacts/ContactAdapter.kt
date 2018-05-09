07a3baedf9363852d5adcdfd283208eb85e4d3d4
kiuchikeisuke <minismahn@yahoo.co.jp>
2018-05-09 16:06:33 +0900

    fix checkbox not work

--- app/src/main/kotlin/jp/ne/nissing/rakutencall/presenter/contacts/ContactAdapter.kt
+++ app/src/main/kotlin/jp/ne/nissing/rakutencall/presenter/contacts/ContactAdapter.kt
@@ -4,7 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.CheckBox
 import android.widget.TextView
 import jp.ne.nissing.rakutencall.R
 import jp.ne.nissing.rakutencall.data.entity.contact.Contact
@@ -23,17 +23,17 @@ class ContactAdapter(var entities: List<ContactDto>, val onClickEvent: (Int, Con
 
 
     inner class ContactViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-        private val checkBoxImageView: ImageView by lazy<ImageView> { itemView!!.findViewById(R.id.checkBoxImageView) }
+        private val checkBox: CheckBox by lazy<CheckBox> { itemView!!.findViewById(R.id.checkBox) }
         private val displayNameTextView: TextView by lazy<TextView> { itemView!!.findViewById(R.id.displayNameTextView) }
         private val telNumberTextView: TextView by lazy<TextView> { itemView!!.findViewById(R.id.telNumberTextView) }
 
         fun onBindViewHolder(entity: ContactDto, position: Int) {
-            checkBoxImageView.isSelected = entity.isSelected
+            checkBox.isChecked = entity.isSelected
             displayNameTextView.text = entity.contact.displayName
             telNumberTextView.text = entity.contact.phoneNumberString
             itemView.setOnClickListener {
-                checkBoxImageView.isSelected = !checkBoxImageView.isSelected
-                onClickEvent(position, entity.contact, checkBoxImageView.isSelected)
+                checkBox.isChecked = !checkBox.isChecked
+                onClickEvent(position, entity.contact, checkBox.isChecked)
             }
         }
     }
